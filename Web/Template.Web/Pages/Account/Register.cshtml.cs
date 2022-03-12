a466ea73cb6b29645a163152c4e87d1b847a4555
gKerkelov03 <gkerkelov03@abv.bg>
2022-03-12 23:12:35 +0200

    started working on custom navigation bar inspired by lazar's frontend

--- Web/Template.Web/Pages/Account/Register.cshtml.cs
+++ Web/Template.Web/Pages/Account/Register.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Template.Data.Models;
@@ -7,10 +8,21 @@ namespace Template.Web.Pages
 {
     public class RegisterPage : PageModel
     {
-        public RegisterPage(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
-        {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly IEmailSender _emailSender;
 
+        public RegisterPage(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IUserStore<IdentityUser> userStore, IUserEmailStore<IdentityUser> emailStore, IEmailSender emailSender)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = emailStore;
+            _emailSender = emailSender;
         }
+
         public void OnGet()
         {
         }
